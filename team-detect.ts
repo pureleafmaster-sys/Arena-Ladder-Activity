c0068637ffcd68aaab8b8c6c1648d5b93f2a69fa
pureleafmaster-sys <pureleafmaster@gmail.com>
2026-05-02 17:10:46 -0400

    Delete team-detect.ts

--- team-detect.ts
+++
@@ -1,2 +0,0 @@
-export type Bracket="2v2"|"3v3"|"5v5";
-export type ActivityPlayer={playerId:string;rank:number|null;rankDelta:number;name:string;realm:string;realmSlug:string;faction:string;race:string;className:string;spec:string;bracket:Bracket;wins:number;losses:number;rating:number;ratingDelta:number;winsDelta:number;lossesDelta:number;gamesDelta:number;trackedMinutesAgo:number|null;team:string[];teamConfidence:number;session:string;activityStatus:string;};
